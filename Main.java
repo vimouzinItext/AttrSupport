98212eec0842e951e47144ba2b2f84d2c24f4485
vimouzinItext <68127958+vimouzinItext@users.noreply.github.com>
2021-06-28 17:23:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         LicenseKey.loadLicenseFile("iTextKey.xml");
-        PdfDocument pdfDocument = new PdfDocument(new PdfWriter("output.pdf"));
-        HtmlConverter.convertToPdf(new FileInputStream("sample_original (2).html"),pdfDocument);
+        PdfDocument pdfDocument = new PdfDocument(new PdfWriter("cmp_checkAttrFunctionWithTypeTest.pdf"));
+        HtmlConverter.convertToPdf(new FileInputStream("inputHtml.html"),pdfDocument);
     }
 }
