43aadd8289ec0cb8a05b20a859b560a5798d2847
vimouzinItext <68127958+vimouzinItext@users.noreply.github.com>
2021-06-28 17:18:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using iText.Html2pdf;
+using iText.License;
+using System.IO;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LicenseKey.LoadLicenseFile("iTextLicenseKey.xml");
+            using (FileStream htmlSource = File.Open("input.html", FileMode.Open))
+            using (FileStream pdfDest = File.Open("output.pdf", FileMode.OpenOrCreate))
+            {
+                ConverterProperties converterProperties = new ConverterProperties();
+                HtmlConverter.ConvertToPdf(htmlSource, pdfDest, converterProperties);
+            }
+        }
+    }
+}
