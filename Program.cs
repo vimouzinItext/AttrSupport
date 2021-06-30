612fd7b237fcb2cfc4e55128b2e0d29007090873
vimouzinItext <68127958+vimouzinItext@users.noreply.github.com>
2021-06-30 09:51:58 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using iText.Html2pdf;
+using System.IO;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            using (FileStream htmlSource = File.Open("inputHtml.html", FileMode.Open))
+            using (FileStream pdfDest = File.Open("output.pdf", FileMode.OpenOrCreate))
+            {
+                ConverterProperties converterProperties = new ConverterProperties();
+                HtmlConverter.ConvertToPdf(htmlSource, pdfDest, converterProperties);
+            }
+        }
+    }
+}
